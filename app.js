43ecff54b8580b96fe88b1520aa1d99b8d78dfdd
Ivymumbi <ivymumbi078@gmail.com>
2026-03-17 19:31:31 +0300

    updated JS by adding set interval

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-let nairobiElement = document.querySelector("#nairobi");
-let nairobiDateElement = nairobiElement.querySelector(".date");
-let nairobiTimeElement = nairobiElement.querySelector(".time");
-let nairobiTime = moment().tz("Africa/Nairobi");
+function updateTime() {
+  let nairobiElement = document.querySelector("#nairobi");
+  let nairobiDateElement = nairobiElement.querySelector(".date");
+  let nairobiTimeElement = nairobiElement.querySelector(".time");
+  let nairobiTime = moment().tz("Africa/Nairobi");
 
-nairobiDateElement.innerHTML = nairobiTime.format("ddd Do MMMM YYYY");
-nairobiTimeElement.innerHTML = nairobiTime.format(
-  "h:mm:ss [<small>]A[</small>]",
-);
+  nairobiDateElement.innerHTML = nairobiTime.format("ddd Do MMMM YYYY");
+  nairobiTimeElement.innerHTML = nairobiTime.format(
+    "h:mm:ss [<small>]A[</small>]",
+  );
 
-let cairoElement = document.querySelector("#cairo");
-let cairoDateElement = cairoElement.querySelector(".date");
-let cairoTimeElement = cairoElement.querySelector(".time");
-let cairoTime = moment().tz("Africa/Cairo");
+  let cairoElement = document.querySelector("#cairo");
+  let cairoDateElement = cairoElement.querySelector(".date");
+  let cairoTimeElement = cairoElement.querySelector(".time");
+  let cairoTime = moment().tz("Africa/Cairo");
 
-cairoDateElement.innerHTML = cairoTime.format("ddd Do MMMM YYYY");
-cairoTimeElement.innerHTML = cairoTime.format("h:mm:ss [<small>]A[</small>]");
+  cairoDateElement.innerHTML = cairoTime.format("ddd Do MMMM YYYY");
+  cairoTimeElement.innerHTML = cairoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
