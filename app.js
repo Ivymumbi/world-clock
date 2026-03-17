0da615a94b003272cc3aa5accd99b7894923304d
Ivymumbi <ivymumbi078@gmail.com>
2026-03-17 19:20:27 +0300

    added moment js to my file and JS for functioning

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+let nairobiElement = document.querySelector("#nairobi");
+let nairobiDateElement = nairobiElement.querySelector(".date");
+let nairobiTimeElement = nairobiElement.querySelector(".time");
+let nairobiTime = moment().tz("Africa/Nairobi");
 
+nairobiDateElement.innerHTML = nairobiTime.format("ddd Do MMMM YYYY");
+nairobiTimeElement.innerHTML = nairobiTime.format(
+  "h:mm:ss [<small>]A[</small>]",
+);
+
+let cairoElement = document.querySelector("#cairo");
+let cairoDateElement = cairoElement.querySelector(".date");
+let cairoTimeElement = cairoElement.querySelector(".time");
+let cairoTime = moment().tz("Africa/Cairo");
+
+cairoDateElement.innerHTML = cairoTime.format("ddd Do MMMM YYYY");
+cairoTimeElement.innerHTML = cairoTime.format("h:mm:ss [<small>]A[</small>]");
